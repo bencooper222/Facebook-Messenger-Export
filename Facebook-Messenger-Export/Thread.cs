7881d9814fbd2844a78a32beaef4f74e2d51bd3e
Benjamin Cooper <bencooper222@gmail.com>
2017-04-29 03:19:39 -0500

    Built out thread class methods

--- Facebook-Messenger-Export/Thread.cs
+++ Facebook-Messenger-Export/Thread.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace Facebook_Messenger_Export
 {
@@ -19,6 +21,46 @@ namespace Facebook_Messenger_Export
         {
             Messages = new List<Message>();
             Participants = participants;
+        }
+
+        public Thread(HtmlDocument thread)
+        {
+            HtmlNode body = thread.DocumentNode.ChildNodes.First().ChildNodes.ToList()[0]; // annoying to extract
+            Participants = CreateListOfParticipants(body.FirstChild.InnerText);
+            
+
+        }
+
+        /// <summary>
+        /// Helper method to construct Participants object
+        /// </summary>
+        /// <param name="text">Plain text from Facebook</param>
+        /// <returns>List of Person objects for each ID</returns>
+        private List<Person> CreateListOfParticipants(string text)
+        {
+            string[] ids = Regex.Split(text, ", ");
+            //DateTime time = new DateTime();
+           // time.   
+            List<Person> rtn = new List<Person>();
+            foreach(string s in ids)
+            {
+                rtn.Add(new Person(s.Trim()));
+            }
+            return rtn;
+        }
+
+
+        /// <summary>
+        /// Creates a Message object from the two HTML tags necessary to do so
+        /// See the README for an explanation so you know what I'm talking about
+        /// </summary>
+        /// <param name="messageHeader">The tag with class message_header</param>
+        /// <param name="pStuff">Stuff in the p (the actual message)</param>
+        /// <returns>A nice message object with the information</returns>
+        private Message AddMessage(HtmlNode messageHeader, HtmlNode paragraphTag)
+        {
+            //   Message message = new Message(paragraphTag.InnerHtml,)
+            return new Message();
         }
 
 
