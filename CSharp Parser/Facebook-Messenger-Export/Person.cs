f2e8510476bce360bf2364ef820707cb5f8278fb
Benjamin Cooper <bencooper222@gmail.com>
2017-05-08 18:18:19 -0500

    General changes

--- CSharp Parser/Facebook-Messenger-Export/Person.cs
+++ CSharp Parser/Facebook-Messenger-Export/Person.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Facebook_Messenger_Export
 {
     class Person
     {
 
-        string Name { get; }
-        string UID { get; }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string UID { get; }
         public int Count { // total messages sent
             get
             {
@@ -18,10 +30,11 @@ namespace Facebook_Messenger_Export
             }
         }
         private int count;
+        private string name;
 
         public Person(string uid,string name = null)
         {
-            Name = name;
+            this.name = name;
             UID = uid;
         }
 
@@ -31,9 +44,46 @@ namespace Facebook_Messenger_Export
         public void AddMessageCount()
         {
             count++;
-            
+    //     DotNetOpenAuth.OAuth.ChannelElements.   
         }
 
-        
+        /// <summary>
+        /// Use Facebook Graph ID to get Name
+        /// </summary>
+        /// <returns>Boolen for success or failure</returns>
+        public bool GetName()
+        {
+          //  StreamWriter logger = new StreamWriter(ConfigurationManager.AppSettings["logLocation"]+ @"\graph-api-logs.txt");
+            try
+            {
+                var url = "https://graph.facebook.com/v2.9/";
+                var client = new RestClient(url + UID + "?oauth_token=" + ConfigurationManager.AppSettings["fbAccessToken"] + "&oauth_signature_method=HMAC-SHA1 " + "&oauth_timestamp=1494281247&oauth_nonce=NQBv1w&oauth_version=1.0&oauth_signature=fBcO9HTiU4gG%20FEHjnTRM5bFAbc%3D");
+                var request = new RestRequest(Method.GET);
+                request.Timeout = 1000;
+                request.AddHeader("cache-control", "no-cache");
+                JObject response = JObject.Parse(client.Execute(request).Content);
+
+                name = response["name"].ToString();
+                File.AppendAllText(ConfigurationManager.AppSettings["logLocation"] + @"\graph-api-logs.txt", DateTime.UtcNow + ": SUCCESS: " + UID + ": " + Name + Environment.NewLine);
+              
+            }
+            catch (Exception e)
+            {
+                File.AppendAllText(ConfigurationManager.AppSettings["logLocation"] + @"\graph-api-logs.txt", DateTime.UtcNow + ": FAILURE: " + UID + ": " + e + Environment.NewLine);
+
+   
+           
+                return false;   
+            }
+            //logger.Close();
+            
+
+            return true;
+
+         
+
+        }
+
+
     }
 }
