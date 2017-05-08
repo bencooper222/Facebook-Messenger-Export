00c4a3d7354ba903c868ec982a76d1d9fd087bec
Benjamin Cooper <bencooper222@gmail.com>
2017-05-08 18:17:40 -0500

    Created basic utilities

---
+++ CSharp Parser/Facebook-Messenger-Export/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook_Messenger_Export
+{
+    class Utilities
+    {
+
+
+        public static void AddJsonToWebRequest(HttpWebRequest req, JObject param)
+        {
+            IList<string> keys = param.Properties().Select(p => p.Name).ToList();
+
+          foreach(string s in keys)
+            {
+                req.Headers.Add(s, param[s].ToString());
+           //     Console.WriteLine(s + " " + param[s]);
+            }
+        }
+    }
+}
