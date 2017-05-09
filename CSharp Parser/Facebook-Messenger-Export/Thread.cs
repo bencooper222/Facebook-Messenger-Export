b56dca207164ee703f3541b3c77846dba5b04a8c
Benjamin Cooper <bencooper222@gmail.com>
2017-05-09 01:00:31 -0500

    Better lookup from Facebook for messages

--- CSharp Parser/Facebook-Messenger-Export/Thread.cs
+++ CSharp Parser/Facebook-Messenger-Export/Thread.cs
@@ -15,41 +15,51 @@ namespace Facebook_Messenger_Export
         public List<Message> Messages { get; } // is this the best data structure?
         public List<Person> Participants { get; }
         public int UID { get; }
+        private IdLookupFactory Lookup;
 
         /// <summary>
         /// Default contstructor
         /// </summary>
         /// <param name="participants">The list of participants (parsed elsewhere)</param>
-        public Thread(List<Person> participants)
+        public Thread(List<Person> participants,IdLookupFactory lookup )
         {
             Messages = new List<Message>();
             Participants = participants;
+            Lookup = lookup;
         }
 
-        public Thread(HtmlDocument thread, int id)
+        public Thread(HtmlDocument thread, int id, IdLookupFactory lookup)
         {
             UID = id;
+            Lookup = lookup;
+
+            bool DoLookup = false; // should query for names?
+            if(Lookup != null)
+            {
+                DoLookup = true;
+            }
 
 
 
             HtmlNode body = thread.DocumentNode.FirstChild.ChildNodes.Where(w => w.Name == "body").ToList()[0];
             List<HtmlNode> nodes = body.ChildNodes.Where(w => w.Name != "#text").ToList(); // eliminates whitespace text nodes
 
-            Participants = CreateListOfParticipants(body.FirstChild.InnerText);
+            Participants = CreateListOfParticipants(body.FirstChild.InnerText,DoLookup);
 
             Messages = new List<Message>();
             for (int i= 0; i< nodes.Count; i+=2){
                 AddMessage(nodes[i].ChildNodes[1], nodes[i + 1]);
             }
-
+            Messages.Reverse(); // because FB gives it most recent to least recent
         }
 
         /// <summary>
         /// Helper method to construct Participants object
         /// </summary>
         /// <param name="text">Plain text from Facebook</param>
+        ///  <param name="getNames">Whether or not to make requests to Facebook for all the names</param>
         /// <returns>List of Person objects for each ID</returns>
-        private List<Person> CreateListOfParticipants(string text)
+        private List<Person> CreateListOfParticipants(string text, bool getNames = false)
         {
             string[] ids = Regex.Split(text, ", ");
             //DateTime time = new DateTime();
@@ -57,10 +67,19 @@ namespace Facebook_Messenger_Export
             List<Person> rtn = new List<Person>();
             foreach(string s in ids)
             {
-                rtn.Add(new Person(s.Trim()));
+                string uidNoWhitespace = s.Trim();
+                string uid = uidNoWhitespace.Remove(uidNoWhitespace.Length - 13, 13);
+                Person person = new Person(uid);
+                if (getNames)
+                {
+                    person.Name = Lookup.GetName(uid);
+                }
+                
+                rtn.Add(person);
             }
             return rtn;
         }
+
 
 
         /// <summary>
@@ -75,14 +94,30 @@ namespace Facebook_Messenger_Export
             HtmlNodeCollection attributes = messageHeader.ChildNodes;
 
             // first tag
-            string facebookEmail = attributes[0].InnerText;
+            string facebookIdentifier = attributes[0].InnerText; // this could be in the form of UID@facebook.com or just simple first last name
 
             // process time (second tag)
             string rawTime = attributes[1].InnerText;
             Moment time = new Moment(rawTime.Remove(rawTime.Length - 4, 4), rawTime.Remove(0, rawTime.Length - 3));
 
-            Message message = new Message(paragraphTag.InnerText,time,facebookEmail.Remove(facebookEmail.Length-13,13),UID);
-            Messages.Add(message);                           
+            string senderId;
+            string senderName;
+            if (facebookIdentifier.Contains("@"))
+            {
+                // if it's in the form of UID@facebook.com
+                 senderId = facebookIdentifier.Remove(facebookIdentifier.Length - 13, 13);
+                senderName = Lookup.GetName(senderId); 
+            }
+            else
+            {
+                // if it's just first and last name
+                senderId = Lookup.GetUID(facebookIdentifier); // should have already been added at the beginning of the thread
+                senderName = facebookIdentifier;
+            }
+            
+           
+           
+            Messages.Add(new Message(paragraphTag.InnerText, time, UID, senderId, senderName));                           
         }
         
 
