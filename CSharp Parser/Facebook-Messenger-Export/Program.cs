886131c517ecb7788cc3235a2b7e9ec9df570e9d
Benjamin Cooper <bencooper222@gmail.com>
2017-05-09 01:00:55 -0500

    Just testing stuff

--- CSharp Parser/Facebook-Messenger-Export/Program.cs
+++ CSharp Parser/Facebook-Messenger-Export/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using System.Configuration;
 using HtmlAgilityPack;
 using System.Globalization;
+using OAuth;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace Facebook_Messenger_Export
 {
@@ -13,16 +18,22 @@ namespace Facebook_Messenger_Export
     { //http://www.nudoq.org/#!/Packages/HtmlAgilityPack/HtmlAgilityPack
         static void Main(string[] args)
         {
+
+
+            
             
             HtmlDocument doc = new HtmlDocument();
-            doc.Load(@"C:\Users\benco\Documents\Visual Studio 2015\Projects\Facebook-Messenger-Export\CSharp Parser\threads\1.html");
+            string threadId = "29";
+            doc.Load(@"C:\Users\benco\Documents\Visual Studio 2015\Projects\Facebook-Messenger-Export\CSharp Parser\threads\" + threadId + ".html");
 
+            IdLookupFactory factory = new IdLookupFactory();
 
-            Thread test = new Thread(doc,0);
+            Thread test = new Thread(doc,0,factory);
             Console.WriteLine(test.MessagesToJson());
             
             
-            
+           
+
             /*
             doc.Load(ConfigurationManager.AppSettings["messageLocation"]);
 
@@ -44,6 +55,61 @@ namespace Facebook_Messenger_Export
 
             ThreadSeparator separate = new ThreadSeparator(threadContainer);
             separate.WriteThreads();
+        }
+
+        private static void WeirdGetOauth()
+        {
+            //https://stackoverflow.com/questions/4002847/oauth-with-verification-in-net
+            var oauth = new OAuth.Manager();
+            oauth["consumer_key"] = "1891815467765937";
+            oauth["consumer_secret"] = "9b4f44d6f6a3f9369bcfd36bf3a407cb";
+            oauth["token_secret"] = ConfigurationManager.AppSettings["fbAccessToken"];
+
+
+            // now, update twitter status using that access token
+            var appUrl = "https://graph.facebook.com/v2.9/4";
+            var authzHeader = oauth.GenerateAuthzHeader(appUrl, "POST");
+            var request = (HttpWebRequest)WebRequest.Create(appUrl);
+            request.Method = "GET";
+            request.PreAuthenticate = true;
+            request.AllowWriteStreamBuffering = true;
+            //        request.Headers.Add("Authorization", authzHeader);
+
+            string editedAuthHeader = ("{" + authzHeader.Remove(0, 6) + "}").Replace("=", ":");
+            JObject o = JObject.Parse(editedAuthHeader);
+
+            Console.WriteLine(" ");
+            Console.WriteLine(o.ToString());
+
+            Utilities.AddJsonToWebRequest(request, o);
+
+
+            request.Headers.Add("oauth_token", ConfigurationManager.AppSettings["fbAccessToken"]);
+            Console.WriteLine(JsonConvert.SerializeObject(request.Headers));
+
+            //    request.Headers.Remove("Host");
+            //  request.Headers.Remove("Connection");
+            Console.WriteLine(request);
+            try
+            {
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("There's been a problem trying to tweet:" + response.StatusDescription);
+                    }
+                    else
+                    {
+                        Console.Write(response.ToString());
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
         }
     }
 
