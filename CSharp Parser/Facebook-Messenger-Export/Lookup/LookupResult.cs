a3039a36e7b9b1141a7c2380dbde3ec7fb86c86f
Benjamin Cooper <bencooper222@gmail.com>
2017-05-12 01:49:21 -0500

    Created new Lookup class

---
+++ CSharp Parser/Facebook-Messenger-Export/Lookup/LookupResult.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook_Messenger_Export
+{
+    class LookupResult
+    {
+
+        public string Name { get;}
+        public bool IsReal { get; }
+        
+
+        public LookupResult(string name, bool isReal)
+        {
+            Name = name;
+            IsReal = isReal;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode(); // this is an awful idea
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+           
+            if (!(obj is LookupResult))
+            {
+                return false;
+            }
+
+            if (this == (LookupResult)obj)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator ==(LookupResult one, LookupResult two)
+        {
+            if (one.Name == two.Name)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(LookupResult one, LookupResult two)
+        {
+            return !(one == two);
+        }
+    }
+
+    }
+
